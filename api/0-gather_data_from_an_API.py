d9bcfddf4d544dc6616453b8e4826ab2c31d4aa2
lucieruin <lucie.wattez@hotmail.fr>
2023-12-19 10:40:42 +0100

    changed ID by Id

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ using this REST API """
 
-import json
 import requests
 from sys import argv
 
@@ -16,16 +15,16 @@ if __name__ == '__main__':
     user_data = user_response.json()
 
     """ check user's to do list """
-    to_do_response = requests.get(f"{API_URL}/todos?user_ID={argv[1]}")
-    to_do_data = to_do_response.json()
+    todo_response = requests.get(f"{API_URL}/todos?userId={argv[1]}")
+    todo_data = todo_response.json()
 
     """ filter for task completed """
-    completed_task = [task for task in to_do_data if task['completed']]
+    completed_task = [task for task in todo_data if task['completed']]
 
     """ display progression """
     employee_name = user_data["name"]
     num_task_completed = len(completed_task)
-    total_task = len(to_do_data)
+    total_task = len(todo_data)
     print("Employee {} is done with task({}/{}):".format(
         employee_name, num_task_completed, total_task))
 
